ed67a30ddfbcf9420ce0901e875601635101ceb3
swaraj bachu <rajswaraj.r@gmail.com>
2024-03-28 21:42:03 +0530

    multiple concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::{TcpListener, TcpStream}};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
+
         match stream {
-            Ok(_stream) => {
+            Ok((_stream,_)) => {
                 println!("accepted new connection");
                 handle_incoming_connection(_stream)
             }
@@ -22,15 +24,19 @@ fn main() {
 }
 
 fn handle_incoming_connection(mut stream: TcpStream) {
-    let mut buf = [0; 512];
-    // read 1024 bytes at a time
-    loop {
-        let read_count = stream.read(&mut buf).unwrap();
-        if read_count == 0 {
-            break;
+
+    tokio::spawn(async move {
+        let mut buf = [0; 512];
+        // read 1024 bytes at a time
+        loop {
+            let read_count = stream.read(&mut buf).await.unwrap();
+            if read_count == 0 {
+                break;
+            }
+            println!("received {} bytes", read_count);
+            stream.write(b"+PONG\r\n").await.unwrap();
         }
-        println!("received {} bytes", read_count);
-        stream.write(b"+PONG\r\n").unwrap();
-    }
+    });
+    
 
 }
